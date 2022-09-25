caad457a06f792b4b14a9e8f8b6fe3d73e1cfc05
nikhiljaitak <niks4u93@gmail.com>
2022-09-25 11:22:55 +0530

    quiz-2

--- plot_digits_classification.py
+++ plot_digits_classification.py
@@ -8,8 +8,12 @@ from sklearn import datasets, svm, metrics
 from sklearn.model_selection import train_test_split
 
 
+#GAMMAS = [0.005,0.001]
+#Clist=[0.1,0.5]
+
 GAMMAS = [0.005,0.001,0.05,0.01,0.1,0.5]
 Clist=[0.1,0.5,1,3,5]
+
 C=0.5
 GAMMA=0.005
 train_frac = 0.8
@@ -42,6 +46,11 @@ def reshapeImages(inputDigits):
 def splitting_model_performance(reshapedImagesarg, inputDigitsarg, best_dev_Accuracy, best_hyper_params, gamma, c):
 
     dev_test_frac = 1-train_frac
+
+    X_train_, X_dev_test_, y_train_, y_dev_test_ = train_test_split(
+        reshapedImagesarg, inputDigitsarg.target, test_size=train_frac, shuffle=True
+                                                                  )
+
     X_train, X_dev_test, y_train, y_dev_test = train_test_split(
         reshapedImagesarg, inputDigitsarg.target, test_size=dev_test_frac, shuffle=True
                                                                   )
@@ -59,10 +68,11 @@ def splitting_model_performance(reshapedImagesarg, inputDigitsarg, best_dev_Accu
 
 #PART: Train model
 # Learn the digits on the train subset
-    clf.fit(X_train, y_train)
+    clf.fit(X_train_, y_train_)
 
 #PART: Get test set predictions
 # Predict the value of the digit on the test subset
+    predicted_train = clf.predict(X_dev_test_)
     predicted_test = clf.predict(X_test)
     predicted_dev = clf.predict(X_dev)
 
@@ -83,19 +93,21 @@ def splitting_model_performance(reshapedImagesarg, inputDigitsarg, best_dev_Accu
         ax.set_title(f"Prediction: {prediction}")
 
 #PART: Compute evaluation metrics on dev and test
+    train_accuracy = metrics.accuracy_score(predicted_train, y_dev_test_ )
     dev_Accuracy = metrics.accuracy_score(predicted_dev, y_dev )
     test_accuracy = metrics.accuracy_score(predicted_test,y_test)
 
-    print('dev accuracy: ',metrics.accuracy_score(predicted_dev, y_dev ))
-    print('test accuracy: ',metrics.accuracy_score(predicted_test,y_test))
+    print('Train accuracy: ', train_accuracy)
+    print('Dev accuracy: ', dev_Accuracy)
+    print('test accuracy: ', test_accuracy)
 
     if dev_Accuracy > best_dev_Accuracy:
-        return dev_Accuracy, hyper_params
+        return dev_Accuracy, hyper_params, dev_Accuracy, test_accuracy, train_accuracy
     else:
-        return best_dev_Accuracy, best_hyper_params
+        return best_dev_Accuracy, best_hyper_params, dev_Accuracy, test_accuracy, train_accuracy
 
 import pandas as pd
-df=pd.DataFrame(columns=['gamma','c'])
+df=pd.DataFrame(columns=[ 'train acc', 'dev acc', 'test acc'])
 reshapedImages=reshapeImages(digits)
 parametersList=[]
 index=0
@@ -109,10 +121,34 @@ for gamma in GAMMAS:
 print(len(df))
 
 for index,row in df.iterrows():
-    best_dev_Accuracy, best_hyper_params = splitting_model_performance(reshapedImages, digits,best_dev_Accuracy,best_hyper_params,row['gamma'],row['c'])
+    best_dev_Accuracy, best_hyper_params,dev_Accuracy, test_accuracy, train_accuracy = splitting_model_performance(reshapedImages, digits,best_dev_Accuracy,best_hyper_params,row['gamma'],row['c'])
+    df.at[index,'dev acc']=dev_Accuracy
+    df.at[index,'test acc']=test_accuracy
+    df.at[index,'train acc']=train_accuracy
     print(best_dev_Accuracy, best_hyper_params)
 
 print('best parameters and accuracy ', best_dev_Accuracy, best_hyper_params)
 print('digits shape of pixels:',digits.images.shape)
+print(df.head())
 
-    
+df=df[[ 'train acc', 'dev acc', 'test acc']]
+
+df.head(10)
+
+print('Mean train acc:',df['train acc'].mean())
+print('Mean dev acc:',df['dev acc'].mean())
+print('Mean test acc:',df['test acc'].mean())
+ 
+print('Median train acc:',df['train acc'].median())
+print('Median dev acc:',df['dev acc'].median())
+print('Median test acc:',df['test acc'].median())
+
+print('Min train acc:',df['train acc'].min())
+print('Min dev acc:',df['dev acc'].min())
+print('Min test acc:',df['test acc'].min())
+
+print('Max train acc:',df['train acc'].max())
+print('Max dev acc:',df['dev acc'].max())
+print('Max test acc:',df['test acc'].max())
+
+print(best_dev_Accuracy, best_hyper_params)
